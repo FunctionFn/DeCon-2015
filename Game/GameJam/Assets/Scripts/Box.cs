d89b468153eb9ef82a061abd973c38e05fa4765f
DampChinchilla <sam.gilroy2@gmail.com>
2015-10-23 22:21:07 -0500

    Added Unity Files (Box Scene)
    This is the box file we used for explaining Unity, and our Unity Project

---
+++ Game/GameJam/Assets/Scripts/Box.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Box : MonoBehaviour {
+	public Rigidbody2D rb;
+	public float SPEED;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.W))
+		{
+			rb.AddForce(new Vector2(0, SPEED));
+		}
+	}
+}

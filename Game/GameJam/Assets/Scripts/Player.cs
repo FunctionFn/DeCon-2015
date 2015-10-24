47d3ef5dd271d8accd0aaedcf17c976a8858278c
FunctionFn <dfernow@ymail.com>
2015-10-24 09:34:58 -0500

    Added player scene and basic player controls
    Horizontal Movement, Jumping (no grounded check)

---
+++ Game/GameJam/Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+   
+    public Rigidbody2D rb;
+
+    public float speed;
+    public float jumpHeight;
+
+    float moveDirection;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveDirection = Input.GetAxis("Horizontal");
+
+        
+
+        ControllerUpdate();
+	}
+
+
+    void ControllerUpdate()
+    {
+        Move();
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection * speed, rb.velocity.y);
+    }
+
+    void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+    }
+
+    
+
+}

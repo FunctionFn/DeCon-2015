0a2db052608595eaa82f3c488ad5b16676ee2fdd
DampChinchilla <sam.gilroy2@gmail.com>
2015-10-24 11:08:35 -0500

    Created Enemy Spawner
    Created the script that will spawn enemies into the level

---
+++ Game/GameJam/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLeft : MonoBehaviour {
+	public Rigidbody2D rb;
+	public float SPEED;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			rb.AddForce(new Vector2(-SPEED, 0));
+		
+	}
+}
---
+++ Game/GameJam/Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+    public GameObject enemy;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+    public float spawnCooldown = 1;
+    private float timeUntilSpawn = 0;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeUntilSpawn = Time.deltaTime;
+        if (timeUntilSpawn <= 0)
+        {
+            int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+            Instantiate(enemy, spawnPoints[spawnPointIndex].position, Quaternion.identity);
+            timeUntilSpawn = spawnCooldown;
+        }
+	}
+}

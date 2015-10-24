25bcc378278818815c8fedcc213688303cb3a108
DampChinchilla <sam.gilroy2@gmail.com>
2015-10-24 16:32:43 -0500

    made spawning more random

--- Game/GameJam/Assets/Scripts/TerrainSpawner.cs
+++ Game/GameJam/Assets/Scripts/TerrainSpawner.cs
@@ -1,15 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Spawner : MonoBehaviour {
-    public GameObject enemy;
-    public Transform[] spawnPoints;
-    protected float spawnCooldown = 1;
-    protected float timeUntilSpawn = 0;
+public class TerrainSpawner : Spawner {
 
 	// Use this for initialization
 	void Start () {
-
+	
 	}
 	
 	// Update is called once per frame
@@ -20,8 +16,8 @@ public class Spawner : MonoBehaviour {
             int spawnPointIndex = Random.Range(0, spawnPoints.Length);
 
             Instantiate(enemy, spawnPoints[spawnPointIndex].position, Quaternion.identity);
-            spawnCooldown = Random.Range(.5f, 2);
+            spawnCooldown = Random.Range(2, 15);
             timeUntilSpawn = spawnCooldown;
         }
-	}
+    }
 }

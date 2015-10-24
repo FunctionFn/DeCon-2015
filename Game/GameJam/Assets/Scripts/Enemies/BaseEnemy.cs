63157a78094beb61c9177ab289e71a1415d9fb75
FunctionFn <dfernow@ymail.com>
2015-10-24 11:07:36 -0500

    Added basic enemy
    just moves right, base class for future enemies

---
+++ Game/GameJam/Assets/Scripts/Enemies/BaseEnemy.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseEnemy : MonoBehaviour {
+
+    public enum Direction : int { Left = -1, Right = 1};
+
+    public Rigidbody2D rb;
+
+    public float speed;
+    public Direction dir;
+
+    public float damage;
+    public float startingHealth;
+
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MovementUpdate();
+	}
+
+    void MovementUpdate()
+    {
+        rb.velocity = new Vector2((int)dir * speed, rb.velocity.y);
+    }
+}

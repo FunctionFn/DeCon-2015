d5fd0b1fc9fc7cc62adf785227004525ae9fd85c
FunctionFn <dfernow@ymail.com>
2015-10-24 19:21:38 -0500

    added maincamera movement

---
+++ Game/GameJam/Assets/Scripts/MainCamera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCamera : MonoBehaviour {
+
+    public Rigidbody2D rb;
+
+    public float speed;
+    public float speedIncrement;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        speed += speedIncrement;
+        Move();
+	}
+
+    void Move()
+    {
+        rb.velocity = new Vector2(speed, rb.velocity.y);
+    }
+
+}

ba8b4c0e29e4f0d3da3b01a0df5533088a8e886f
FunctionFn <dfernow@ymail.com>
2015-10-25 12:47:47 -0500

    Changed spawning behavior to ramp up over time

--- Game/GameJam/Assets/Scripts/Spawner.cs
+++ Game/GameJam/Assets/Scripts/Spawner.cs
@@ -6,8 +6,14 @@ public class Spawner : MonoBehaviour {
     public Transform[] spawnPoints;
     protected float spawnCooldown;
     protected float timeUntilSpawn;
+
+    
     public float spawnMin;
     public float spawnMax;
+
+    public float spawnMinFloor;
+    public float spawnMaxFloor;
+    public float spawnDecrement;
 
 	// Use this for initialization
 	void Awake () {
@@ -17,14 +23,26 @@ public class Spawner : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        
         timeUntilSpawn -= Time.deltaTime;
         if (timeUntilSpawn <= 0)
         {
             int spawnPointIndex = Random.Range(0, spawnPoints.Length);
 
             Instantiate(enemy, spawnPoints[spawnPointIndex].position, Quaternion.identity);
-            spawnCooldown = Random.Range(spawnMin, spawnMax);
+            spawnCooldown = Random.Range(spawnMin * 1.0f, spawnMax * 1.0f);
             timeUntilSpawn = spawnCooldown;
+            if(spawnMin > spawnMinFloor)
+                spawnMin -= spawnDecrement;
+
+            if (spawnMin < spawnMinFloor)
+                spawnMin = spawnMinFloor;
+
+            if(spawnMax > spawnMaxFloor)
+                spawnMax -= spawnDecrement;
+
+            if (spawnMax < spawnMinFloor)
+                spawnMax = spawnMinFloor;
         }
 	}
 }

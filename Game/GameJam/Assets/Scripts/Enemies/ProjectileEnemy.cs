9761dbbe5a677ab1310c2756d445bd8e4607e8e9
DampChinchilla <sam.gilroy2@gmail.com>
2015-10-24 22:08:01 -0500

    Projectile Enemy created

---
+++ Game/GameJam/Assets/Scripts/Enemies/ProjectileEnemy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileEnemy : BaseEnemy {
+    public GameObject projectile;
+    protected float spawnCooldown;
+    protected float timeUntilSpawn;
+    // Use this for initialization
+    void Awake()
+    {
+        spawnCooldown = 1;
+        timeUntilSpawn = spawnCooldown;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        timeUntilSpawn -= Time.deltaTime;
+        if (timeUntilSpawn <= 0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeUntilSpawn = spawnCooldown;
+        }
+        base.Update();
+	}
+}
